b7b05e356d1d1cac524810c99b26f56699bdf685
danielwiratman <danielwiratman@gmail.com>
2022-02-24 13:49:06 +0700

    second commit

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo" width={300} />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello <strong>There!</strong>
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Open Youtube
+          Open MyApp
         </a>
       </header>
     </div>
